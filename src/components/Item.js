58026217d880bb0140fc244019f9e22e0db5d8d3
Amos Langat <amos.langat@student.moringaschool.com>
2022-09-22 23:31:49 +0300

    complete assignment

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React,{useState}  from "react";
 
 function Item({ name, category }) {
+
+  const [inCart, setInCart] = useState(false)
+
+  const addToCart = ()=>{
+    setInCart(true);
+    
+  }
+
   return (
-    <li className="">
+    <li className={inCart ? 'in-cart': null}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={addToCart} className="add">Add to Cart</button>
     </li>
   );
 }
